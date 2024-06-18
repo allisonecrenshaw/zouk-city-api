9bc21d1b744bf2f3795df4253ac70d2a037d4d08
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-06-18 16:06:11 -0400

    New base entity created and columns of event defined.

--- src/event/event.entity.ts
+++ src/event/event.entity.ts
@@ -1,10 +1,21 @@
-import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  TableInheritance,
-} from 'typeorm';
+import { Entity, Column, TableInheritance } from 'typeorm';
+import { BaseEntity } from '../utils/base.entity';
 
-@Entity()
+@Entity({ name: 'event' })
 @TableInheritance({ column: { type: 'varchar', name: 'type' } })
-export class Event {}
+export class Event extends BaseEntity {
+  @Column({ nullable: false })
+  type: string;
+
+  @Column({ nullable: false })
+  title: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ nullable: false })
+  mainWebsiteUrl: string;
+
+  @Column({ nullable: true })
+  registrationUrl: string;
+}
---
+++ src/utils/base.entity.ts
@@ -0,0 +1,22 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ name: 'created_date' })
+  createdDate: Date;
+
+  @UpdateDateColumn({ name: 'updated_date' })
+  updatedDate: Date;
+
+  @DeleteDateColumn({ name: 'deleted_date' })
+  deletedDate: Date;
+}
