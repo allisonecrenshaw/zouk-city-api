1d848fdb61e74c8e9f2ee987be36f7efb8a2b921
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-06-24 19:10:41 -0400

    Clearing out entities to start implementing new ERD.

--- src/event/event.entity.ts
+++ src/event/event.entity.ts
@@ -1,21 +1,12 @@
 import { Entity, Column, TableInheritance } from 'typeorm';
 import { BaseEntity } from '../utils/base.entity';
 
-@Entity({ name: 'event' })
+@Entity()
 @TableInheritance({ column: { type: 'varchar', name: 'type' } })
 export class Event extends BaseEntity {
-  @Column({ nullable: false })
-  type: string;
-
-  @Column({ nullable: false })
+  @Column()
   title: string;
 
   @Column()
   description: string;
-
-  @Column({ nullable: false })
-  mainWebsiteUrl: string;
-
-  @Column()
-  registrationUrl: string;
 }
