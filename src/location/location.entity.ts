78544730dda8482c255fea3baa70a51c68f81cbe
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-06-26 09:27:17 -0400

    Adding location table.

---
+++ src/location/location.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, Column, TableInheritance } from 'typeorm';
+import { BaseEntity } from '../utils/base.entity';
+
+@Entity()
+@TableInheritance({ column: { type: 'varchar', name: 'type' } })
+export class Location extends BaseEntity {
+  @Column({ nullable: false })
+  title: string;
+
+  @Column()
+  address_line1: string;
+
+  @Column()
+  address_line2: string;
+
+  @Column({ nullable: false })
+  city: string;
+
+  @Column({ nullable: false })
+  state: string;
+
+  @Column({ nullable: false })
+  country: string;
+
+  @Column()
+  postal_code: string;
+}
