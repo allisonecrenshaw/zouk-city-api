79d93e8cf8ddb60eb7730b39fa0addfd057d99a2
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-06-27 11:10:03 -0400

    Update to data source, trying to fix config to get typeorm generate migration to work.

--- src/utils/config/data-source.ts
+++ src/utils/config/data-source.ts
@@ -1,35 +1,21 @@
 import { DataSource } from 'typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import * as path from 'path';
-import * as fs from 'fs';
-import { EntitySchema } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
 
-async function createDataSource(): Promise<DataSource> {
-  const configService = new ConfigService(); // Initialize ConfigService
+config();
 
-  const entitiesPath = path.join(__dirname, '../../v1');
-  const entityFiles = fs
-    .readdirSync(entitiesPath)
-    .filter((file) => file.endsWith('.entity.ts'));
+const configService = new ConfigService();
 
-  const entities = entityFiles.map((file) => {
-    const entity = require(path.join(entitiesPath, file));
-    return entity.default ? entity.default : entity;
-  });
-
-  return new DataSource({
-    type: 'postgres',
-    host: configService.get<string>('DB_HOST'),
-    port: configService.get<number>('DB_PORT'),
-    username: configService.get<string>('DB_USERNAME'),
-    password: configService.get<string>('DB_PASSWORD'),
-    database: configService.get<string>('DB_NAME'),
-    synchronize: false,
-    logging: false,
-    entities: entities as (Function | string | EntitySchema<any>)[],
-    migrations: ['src/migration/**/*.ts'],
-    subscribers: ['src/subscriber/**/*.ts'],
-  });
-}
-
-export default createDataSource;
+export const interactDataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  synchronize: false,
+  logging: false,
+  entities: ['src/v1/**/*.entity.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+});
